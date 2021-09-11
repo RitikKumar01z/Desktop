91d688b1c7f685859ac8ae62a1a196a4451f8955
Ritik Tiwari <90362832+RitikKumar01z@users.noreply.github.com>
2021-09-11 08:04:24 +0530

    first commit

---
+++ coding cpp python/demo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello word"<<endl;
+    cout<<"hello bsdk"<<endl;
+    cout<<"tu chutiya hai abe idhar patak ke pel dunga bhag bosdike";
+    
+
+    return 0;
+}
---
+++ coding cpp python/practice.py
@@ -0,0 +1,10 @@
+print("hello")
+print('''5*1, 5*2, 5*3
+5*4, 5*5, 5*6
+5*7, 5*8, 5*9''')
+print(5*1, 5*2, 5*3)
+print(5*4, 5*5, 5*6)
+print(5*7, 5*8, 5*9)
+from playsound import playsound
+playsound('D:\\Music\\02_-_Dhup_Chik(wapking.cc).mp3')
+playsound('D:\\Music\\Isq Risk - Mere Brother Ki Dulhan [ChatofWorld.Com].mp3')
